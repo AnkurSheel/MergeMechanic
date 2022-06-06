74e1bb34562ce1dbbd1f1aa4b6c51b636069bed4
Ankur Sheel <ankur.sheel@gmail.com>
2022-06-06 23:26:09 +1200

    generate circle shape

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -10,14 +10,17 @@ public class BoardManager : MonoBehaviour
     private int _height = 3;
 
     [SerializeField]
+    private GameObject _cell;
+
+    [SerializeField]
     private GameObject _tile;
 
     [SerializeField]
-    private List<Sprite> _characters = new List<Sprite>();
+    private float _repeatRate = 2.0f;
 
-    private readonly List<TileElement> FullTiles = new List<TileElement>();
+    private readonly List<GameObject> FullTiles = new List<GameObject>();
 
-    private readonly List<TileElement> EmptyTiles = new List<TileElement>();
+    private readonly List<GameObject> EmptyTiles = new List<GameObject>();
 
     public static BoardManager Instance { get; private set; }
 
@@ -26,8 +29,10 @@ public class BoardManager : MonoBehaviour
     {
         Instance = GetComponent<BoardManager>();
 
-        Vector2 offset = _tile.GetComponent<SpriteRenderer>().bounds.size;
+        Vector2 offset = _cell.GetComponent<SpriteRenderer>().bounds.size;
         CreateBoard(offset.x, offset.y);
+
+        InvokeRepeating(nameof(CreateNewTile), 0.0f, _repeatRate);
     }
 
     private void CreateBoard(float offsetX, float offsetY)
@@ -39,10 +44,30 @@ public class BoardManager : MonoBehaviour
         {
             for (var j = 0; j < _height; j++)
             {
-                var tile = Instantiate(_tile, new Vector3(startX + offsetX * i, startY + offsetY * j, 0), _tile.transform.rotation);
+                var tile = Instantiate(_cell, new Vector3(startX + offsetX * i, startY + offsetY * j, 0), _tile.transform.rotation);
                 tile.transform.parent = transform;
-                EmptyTiles.Add(tile.GetComponentInChildren<TileElement>());
+                EmptyTiles.Add(tile);
             }
+        }
+    }
+
+    private void CreateNewTile()
+    {
+        if (EmptyTiles.Count == 0)
+        {
+            Debug.Log("Game Ended!");
+        }
+        else
+        {
+            var tileElement = EmptyTiles[Random.Range(0, EmptyTiles.Count)];
+            var tile = Instantiate(
+                _tile,
+                tileElement.transform.position,
+                tileElement.transform.rotation,
+                tileElement.transform);
+
+            FullTiles.Add(tileElement);
+            EmptyTiles.Remove(tileElement);
         }
     }
 }
---
+++ Assets/Scripts/TileElement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TileElement : MonoBehaviour
+{
+    [SerializeField]
+    private List<Sprite> _shapes = new List<Sprite>();
+
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        var newSprite = _shapes[0];
+        _spriteRenderer.sprite = newSprite;
+    }
+}
