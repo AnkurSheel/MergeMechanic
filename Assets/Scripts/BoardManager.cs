6e7914b3d3621e043396a8ab58d278eb6eb23e23
Ankur Sheel <ankur.sheel@gmail.com>
2022-06-26 13:58:34 +1200

    :lipstick: rename files

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -3,55 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class BoardManager
+namespace MergeMechanic
 {
-    private readonly List<TileElement> _fullTiles = new List<TileElement>();
-
-    private readonly List<TileElement> _emptyTiles = new List<TileElement>();
-
-    private static BoardManager _instance;
-
-    public static BoardManager Instance => _instance ?? (_instance = new BoardManager());
-
-    public void CreateBoard(
-        int width,
-        int height,
-        Vector3 startPosition,
-        Vector3 tileSize,
-        Func<Vector3, TileElement> instantiateGameObjectFunc)
+    public class BoardManager
     {
-        for (var i = 0; i < width; i++)
+        private readonly List<TileElementMonoBehaviour> _fullTiles = new List<TileElementMonoBehaviour>();
+
+        private readonly List<TileElementMonoBehaviour> _emptyTiles = new List<TileElementMonoBehaviour>();
+
+        private static BoardManager _instance;
+
+        public static BoardManager Instance => _instance ?? (_instance = new BoardManager());
+
+        public void CreateBoard(
+            int width,
+            int height,
+            Vector3 startPosition,
+            Vector3 tileSize,
+            Func<Vector3, TileElementMonoBehaviour> instantiateGameObjectFunc)
         {
-            for (var j = 0; j < height; j++)
+            for (var i = 0; i < width; i++)
             {
-                var tileElement = instantiateGameObjectFunc(new Vector3(startPosition.x + tileSize.x * i, startPosition.y + tileSize.y * j, 0));
-                tileElement.Hide();
-                _emptyTiles.Add(tileElement);
+                for (var j = 0; j < height; j++)
+                {
+                    var tileElement = instantiateGameObjectFunc(new Vector3(startPosition.x + tileSize.x * i, startPosition.y + tileSize.y * j, 0));
+                    tileElement.Hide();
+                    _emptyTiles.Add(tileElement);
+                }
             }
         }
-    }
 
-    public void CreateNewTile()
-    {
-        if (_emptyTiles.Count == 0)
+        public void CreateNewTile()
         {
-            Debug.Log("Game Ended!");
+            if (_emptyTiles.Count == 0)
+            {
+                Debug.Log("Game Ended!");
+            }
+            else
+            {
+                var tileElement = _emptyTiles[Random.Range(0, _emptyTiles.Count)];
+                tileElement.Show();
+
+                _fullTiles.Add(tileElement);
+                _emptyTiles.Remove(tileElement);
+            }
         }
-        else
+
+        public void OnMerge(TileElementMonoBehaviour tileElement)
         {
-            var tileElement = _emptyTiles[Random.Range(0, _emptyTiles.Count)];
-            tileElement.Show();
-
-            _fullTiles.Add(tileElement);
-            _emptyTiles.Remove(tileElement);
+            var tile = _fullTiles.Find(x => x == tileElement);
+            _emptyTiles.Add(tile);
+            _fullTiles.Remove(tile);
+            tileElement.gameObject.SetActive(false);
         }
-    }
-
-    public void OnMerge(TileElement tileElement)
-    {
-        var tile = _fullTiles.Find(x => x == tileElement);
-        _emptyTiles.Add(tile);
-        _fullTiles.Remove(tile);
-        tileElement.gameObject.SetActive(false);
     }
 }
---
+++ Assets/Scripts/BoardMonoBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace MergeMechanic
+{
+    public class BoardMonoBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private int _width = 3;
+
+        [SerializeField]
+        private int _height = 3;
+
+        [SerializeField]
+        private GameObject _cell;
+
+        [SerializeField]
+        private float _repeatRate = 2.0f;
+
+        private void Start()
+        {
+            var spriteSize = _cell.GetComponent<SpriteRenderer>().bounds.size;
+            BoardManager.Instance.CreateBoard(
+                _width,
+                _height,
+                transform.position,
+                spriteSize,
+                InstantiateGameObjectFunc);
+
+            InvokeRepeating(nameof(CreateNewTile), 0.0f, _repeatRate);
+        }
+
+        private TileElementMonoBehaviour InstantiateGameObjectFunc(Vector3 position)
+        {
+            var tile = Instantiate(_cell, position, _cell.transform.rotation);
+            tile.transform.parent = transform;
+            var tileElement = tile.GetComponentInChildren<TileElementMonoBehaviour>();
+            return tileElement;
+        }
+
+        public void CreateNewTile()
+        {
+            BoardManager.Instance.CreateNewTile();
+        }
+    }
+}
