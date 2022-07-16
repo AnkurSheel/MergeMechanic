b552e3d1c20b19826cc5305875b94d45c4c530de
Ankur Sheel <ankur.sheel@gmail.com>
2022-07-17 00:48:12 +1200

    adjust the camera on board generation

--- Assets/Scripts/Editor/BoardGeneratorWindow.cs
+++ Assets/Scripts/Editor/BoardGeneratorWindow.cs
@@ -10,6 +10,8 @@ namespace MergeMechanic.Editor
         private string BasePrefabPath = "Assets/Prefabs/Base.prefab";
 
         private GameObject _tile;
+        private ICameraAdjuster _cameraAdjuster;
+        private Camera _camera;
         private Vector3 _spriteSize;
 
         [MenuItem("Level Editor/Board/Board Generator", false, 1)]
@@ -20,6 +22,8 @@ namespace MergeMechanic.Editor
 
         public void Awake()
         {
+            _cameraAdjuster = new CameraAdjuster();
+            _camera = Camera.main;
             _tile = AssetDatabase.LoadAssetAtPath<GameObject>(BasePrefabPath);
             _spriteSize = _tile.GetComponent<SpriteRenderer>().bounds.size;
         }
@@ -37,6 +41,7 @@ namespace MergeMechanic.Editor
             if (GUILayout.Button("Generate Board"))
             {
                 GenerateBoard();
+                AdjustCamera();
                 Close();
             }
         }
@@ -50,6 +55,12 @@ namespace MergeMechanic.Editor
                 _spriteSize,
                 board.transform,
                 _tile);
+        }
+
+        private void AdjustCamera()
+        {
+            _camera.transform.position = _cameraAdjuster.GetAdjustedPosition(Width);
+            _camera.orthographicSize = _cameraAdjuster.GetAdjustedOrthographicSize(Width);
         }
     }
 }
