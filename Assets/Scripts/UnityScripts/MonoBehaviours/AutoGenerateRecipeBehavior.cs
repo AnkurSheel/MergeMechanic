5c1a8411c1b2cbb743f81f421676d50a91eb8628
Ankur Sheel <ankur.sheel@gmail.com>
2022-07-24 15:44:06 +1200

    auto generate circles

---
+++ Assets/Scripts/UnityScripts/MonoBehaviours/AutoGenerateRecipeBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace MergeMechanic.UnityScripts.MonoBehaviours
+{
+    public class AutoGenerateRecipeBehavior : MonoBehaviour
+    {
+        [SerializeField]
+        private float _repeatRate = 2.0f;
+
+        [SerializeField]
+        private RecipeBehaviour _recipe;
+        
+        private void Start()
+        {
+            
+            InvokeRepeating(nameof(CreateRecipe), 0.0f, _repeatRate);
+        }
+
+        private void CreateRecipe()
+        {
+            _recipe.CreateRecipe(1);
+        }
+    }
+}
